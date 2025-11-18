ab1da57d4e980de71115bb625c787d21b02f584f
j4y-boi <57105859+j4y-boi@users.noreply.github.com>
2025-11-18 21:13:28 +0100

    made a crap cassette generator

--- ijzerboer/scripts/cassette.gd
+++ ijzerboer/scripts/cassette.gd
@@ -5,14 +5,36 @@ extends Node3D
 @export var speed:int = 2
 @export var rotateRight:bool = false
 @export_multiline var title:String = "placeholder placeholder"
-
-func _ready() -> void:
-	labelText.text = title
+	
+func setupTape(currentTape): #jesus dit is een rommel, TODO: make this less crap
+	if currentTape["type"] == 1 and $".".name == "cassettelowpoly":
+		labelText.text = currentTape["title"]
+		speed = currentTape["speed"]
+		rotateRight = !currentTape["rotateright"]
+		$".".visible = true
+	elif currentTape["type"] == 2 and $".".name == "Cassette":
+		labelText.text = currentTape["title"]
+		speed = currentTape["speed"]
+		rotateRight = !currentTape["rotateright"]
+		$".".visible = true
+	else:
+		$".".visible = false
 	
 func _process(delta: float) -> void:
 	if rotateRight:
-		cassette_1.rotation.z += delta*-speed
-		cassette_2.rotation.z += delta*-speed
+		if $".".name == "cassettelowpoly":
+			cassette_1.rotation.x += delta*-speed
+			cassette_2.rotation.y += delta*-speed
+		else:
+			cassette_1.rotation.z += delta*-speed
+			cassette_2.rotation.z += delta*-speed
 	else:
-		cassette_1.rotation.z += delta*speed
-		cassette_2.rotation.z += delta*speed
+		if $".".name == "cassettelowpoly":
+			cassette_1.rotation.x += delta*speed
+			cassette_2.rotation.y += delta*speed
+		else:
+			cassette_1.rotation.z += delta*speed
+			cassette_2.rotation.z += delta*speed
+
+func _on_control_new_tape(currentTape) -> void:
+	setupTape(currentTape)
---
+++ ijzerboer/scripts/cassettegenerator.gd
@@ -0,0 +1,31 @@
+#t maakt me geen schijt uit dat dit hella inefficent is, it works so dont touch it
+extends Control
+signal newTape
+@onready var cassette1: Node3D = $SubViewportContainer/SubViewport/Node3D/cassettelowpoly
+@onready var cassette2: Node3D = $SubViewportContainer/SubViewport/Node3D/Cassette
+
+@export var generatedTapes:Dictionary = { #gvd er is geen betere manier, gl future me frfr 🙏🙏
+	"ost1": { "title": "Ijzerman music\nVOLUME 1", "type" : 1 , "speed" : 2, "rotateright" : true, },
+	"ost2": { "title": "insert very original song title", "type" : 2 , "speed" : 2, "rotateright" : true, },
+}
+
+const TAPETEMPLATE := {	"template": { "title": "if you see this then\nthegamebroke", "type" : 1 , "speed" : 2, "rotateright" : true, },}
+#you HVAE to respect this template or the cassettes will die
+
+var index:int = 0
+var currentTape = {}
+var tape_keys := []
+
+func _ready() -> void:
+	if "template" in generatedTapes:
+		generatedTapes.erase("template")
+	tape_keys = generatedTapes.keys()
+	currentTape = generatedTapes[tape_keys[index]]
+	emit_signal("newTape", currentTape)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("ui_accept"):
+			index = (index + 1) % tape_keys.size()
+			currentTape = generatedTapes[tape_keys[index]]
+			emit_signal("newTape", currentTape)
