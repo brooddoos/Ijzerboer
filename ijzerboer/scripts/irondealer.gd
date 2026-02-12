ef1770f981a3bb6e80e96dccae45de0586809794
Kobe Geens <kobegeens@proton.me>
2026-02-12 23:26:40 +0100

    Bug fixes
    -Put the garage and irondeale collisionshapes on the right spot
    -Added a world boundary
    -Improved naming consistency
    -Added the fireplace sound to the SFX bus
    -Added extra iron spawn location and fixed their height

--- ijzerboer/scripts/irondealer.gd
+++ ijzerboer/scripts/irondealer.gd
@@ -1,12 +1,12 @@
 extends Node3D
-@onready var cash_sfx: AudioStreamPlayer3D = $Area3D/cash_sfx
+@onready var cash: AudioStreamPlayer3D = $Area3D/CashSound
 
 
 func _ready() -> void:
 	$Area3D.entered.connect(_on_body_entered)
 func _on_body_entered():
 	if Gamestate.cargo > 0:
-		cash_sfx.play()
+		cash.play()
 
 	Gamestate.BEF += Gamestate.cargo * randi_range(40,80)
 	$"../UI/Values".update_currency()
