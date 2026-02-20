66d01e4694182f67bb649c880792e32a6c52fa25
Kobe Geens <kobegeens@proton.me>
2026-02-20 21:08:51 +0100

    train + map update
    -worked on train crossings
    -imported current blender map

--- ijzerboer/scripts/train.gd
+++ ijzerboer/scripts/train.gd
@@ -1,24 +1,39 @@
 extends Node3D
 @onready var path := $Path3D
 @onready var pathfollow := $Path3D/PathFollow3D
-@onready var pathlength = path.curve.get_baked_length()
+@onready var pathlength = path.curve.get_baked_length() 
 @onready var train := $Path3D/PathFollow3D/HLE16
 @onready var audiostreamplayer := $Path3D/PathFollow3D/AudioStreamPlayer3D
+@onready var crossinglocation = $Crossing.global_position
 var waiting := false
-
+var wait_time := 30 #in s
+var speed = 40 #in m/s
+var trainlocation
+var distance
+var closed = false
 func _process(delta: float) -> void:
+	trainlocation = $Path3D/PathFollow3D.global_position
 	if waiting:
 		return
-	pathfollow.progress += 40 * delta
+	pathfollow.progress += speed * delta
 	if pathfollow.progress_ratio >= 1.0:
 		pathfollow.progress_ratio = 1.0
 		wait()
-		
+	distance = trainlocation.distance_to(crossinglocation)
+	## barrier
+	if distance < 100 and closed == false:
+		$Crossing/AnimationPlayer.play_backwards("PlaneAction")
+		closed = true
+	elif distance > 100 and closed == true:
+		$Crossing/AnimationPlayer.play("PlaneAction")
+		closed = false
+	else:
+		pass
 func wait():
 	waiting = true
 	train.hide()
 	audiostreamplayer.playing = false
-	await get_tree().create_timer(30).timeout
+	await get_tree().create_timer(wait_time).timeout
 	pathfollow.progress_ratio = 0.0
 	waiting = false
 	train.show()
